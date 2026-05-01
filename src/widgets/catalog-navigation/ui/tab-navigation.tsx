e76ffdd495daa35ffa08573d6871189614772617
Kiryl Kireyeu <kirylkireyeu@gmail.com>
2026-05-01 12:52:51 +0200

    feat: save url between change route

--- src/widgets/catalog-navigation/ui/tab-navigation.tsx
+++ src/widgets/catalog-navigation/ui/tab-navigation.tsx
@@ -1,13 +1,15 @@
 import clsx from 'clsx';
-import { NavLink } from 'react-router';
+import { NavLink, useLocation } from 'react-router';
 
 import type { NavigationT } from '../models/navigation-items';
 import styles from './catalog-navigation.module.css';
 
 const TabNavigation = ({ to, title }: NavigationT) => {
+    const { search } = useLocation();
+
     return (
         <NavLink
-            to={to}
+            to={{ pathname: to, search }}
             className={({ isActive }) => clsx(styles.tab, isActive && styles.active)}
         >
             <span className={styles.label}>{title}</span>
