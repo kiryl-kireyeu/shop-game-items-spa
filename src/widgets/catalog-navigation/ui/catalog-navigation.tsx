cc031597547be1ef1c5a5e873ead393497672fff
Kiryl Kireyeu <kirylkireyeu@gmail.com>
2026-05-01 09:12:33 +0200

    feat: added styles for tab navigation

--- src/widgets/catalog-navigation/ui/catalog-navigation.tsx
+++ src/widgets/catalog-navigation/ui/catalog-navigation.tsx
@@ -1,24 +1,25 @@
 import { NavLink } from 'react-router';
 
-import { navigationItems } from '../models/navigation-items';
+import { navigationItems, type NavigationT } from '../models/navigation-items';
 import styles from './catalog-navigation.module.css';
+import clsx from 'clsx';
 
 const CatalogNavigation = () => {
     return (
         <nav className={styles.root} aria-label="Разделы каталога">
-            {navigationItems.map(({ title, to }) => (
-                <NavLink
-                    className={({ isActive }) =>
-                        isActive ? `${styles.link} ${styles.active}` : styles.link
-                    }
-                    key={to}
-                    to={to}
-                >
-                    <span className={styles.label}>{title}</span>
-                </NavLink>
+            {navigationItems.map(({ to, title }) => (
+                <TabNavigation key={to} title={title} to={to} />
             ))}
         </nav>
     );
 };
 
 export default CatalogNavigation;
+
+export const TabNavigation = ({ to, title }: NavigationT) => {
+    return (
+        <NavLink to={to} className={({ isActive }) => clsx(styles.tab, isActive && styles.active)}>
+            <span>{title}</span>
+        </NavLink>
+    );
+};
